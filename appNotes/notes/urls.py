1c7613e43b86a2e57d8a22f33121b9c55bfa8cfa
IrisNieto9 <iris.nieto.montoya@mi.unc.edu.ar>
2022-12-17 19:47:22 -0300

    Creacion de metodos crud para notas

--- appNotes/notes/urls.py
+++ appNotes/notes/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 
 # Views
-from notes.views import NotesApiView
+from notes.views import NotesApiView,NoteDetailApiView
 
 # Urls
 urlpatterns =[
     path('notes-list/', NotesApiView.as_view(), name= 'notes_lists'),
+    path('note-detail/<int:pk>/', NoteDetailApiView.as_view(), name= 'detail_note'),
 ]
--- appNotes/notes/views.py
+++ appNotes/notes/views.py
@@ -28,4 +28,69 @@ class NotesApiView(APIView):
             data=notes_serializer.data,
             status=status.HTTP_200_OK
         )
+
+    def post(self, request):
+        """Crea un nuevo registro"""
+
+        serializer = NoteSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                {'message': 'Nota creada correctamente'},
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            data=serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    
+
+
+class NoteDetailApiView(APIView):
+
+    def get(self, request, pk):
+        """Retorna un listado con todas las notas almacenadas en la base"""
+
+        note = Notes.objects.get(pk=pk)
+        note_serializer = NoteSerializer(note)
+
+        return Response(
+            data=note_serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def put(self, request, pk):
+        """Modifica una nota"""
+
+        note = Notes.objects.get(pk=pk)
+        serializer = NoteSerializer(note, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                {'message': 'Nota modificada correctamente'},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            data=serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        """Elimina una nota"""
+
+        note = Notes.objects.get(pk=pk)
+        note.delete()
+
+        return Response(
+            {'message': 'Nota eliminada correctamente'},
+            status=status.HTTP_200_OK
+        )
+
+
         
